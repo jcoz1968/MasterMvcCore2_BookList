e3d87c2c499e6126be9ffe8768880aa76dfe0e73
Jeffery Cosby <jcoz1968@hotmail.com>
2018-05-08 19:10:10 -0500

    book model, migration, dbcontext, connectionstring

---
+++ BookList/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BookList.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private DbContextOptions<ApplicationDbContext> _options;
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)  : base(options)
+        {
+            _options = options;
+        }
+
+        
+    }
+}
